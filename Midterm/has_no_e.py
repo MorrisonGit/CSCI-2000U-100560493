7f750009c26095118e72d7fb82e076b8fcfae5f6
Christian <christianmorrison@live.ca>
2015-10-28 09:40:25 -0400

    Midterm: completed has_no_e.py(as much as possible)

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,10 @@
+has_no_e(
+word_count = *
+while word <= word_count:
+	if contains_letter_e:
+	print(False)
+	else
+	    print(True)
+)
+
+#Sorry, Didn't know how to recieve user input, same as previosu question.
