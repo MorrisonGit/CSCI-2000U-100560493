18a297b92674e687a7649e5dbd6b6b4ccac9cc2a
Christian <christianmorrison@live.ca>
2015-10-28 09:33:19 -0400

    Midterm: completed strip.sh (as much as possible)

---
+++ Midterm/strip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Christian Morrison - 100560493
+k=?
+m=?
+wc -l $file | sort | tail -n +26 | head -n -2 | rm -l
+
+#Sorry, both of the porgramming questions stumped me. Don't know how
+# to recieve user input.
