5537d8b530172c1298c89c6acfc4925f5f18666a
Christian <christianmorrison@live.ca>
2015-11-05 16:04:46 -0500

    Assignment 2: completed has_no_e.py

--- Assignments/Assignment-2/has_no_e.py
+++ Assignments/Assignment-2/has_no_e.py
@@ -1,5 +1,12 @@
 def has_no_e(string):
 	for N in string:
-		if (N == "e"):
+		if (N == "e" or N == "E"):
 			return False
 	return True
+
+reader = open("gadsby_stripped.txt")
+
+for string in reader:
+	has_no_e(string)
+
+reader.close()
